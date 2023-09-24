690557fd0bdc589a435641867a999a6c2aed6a23
Rafael <rpererah@gmail.com>
2023-09-24 13:59:21 -0300

    aula02

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProdutoModule } from './produto/produto.module';
 import { UsuarioModule } from './usuario/usuario.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DBConfigService } from './config/db.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsuarioModule, ProdutoModule],
+  imports: [UsuarioModule,
+            ProdutoModule,
+            ConfigModule.forRoot({isGlobal:true}),
+            TypeOrmModule.forRootAsync({useClass:DBConfigService,inject:[DBConfigService]})],
 })
 export class AppModule {}
